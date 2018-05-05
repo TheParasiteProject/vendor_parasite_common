ev-staging/android_vendor_ev_build

soong: add non pie support
Change-Id: Ie9e754d5610249b6791c6efff4115d8918d6a682

soong/soong_config.mk
@@ -4,6 +4,7 @@ evervolv_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Evervolv": {'; \
+	echo '    "Needs_non_pie_support": $(if $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)),true,false),'; \
 	echo '    "Needs_prelink_support": $(if $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)),true,false),'; \
 	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
 	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \

TheParasiteProject/vendor_parasite_common

build/soong/soong_config.mk
@@ -10,6 +10,7 @@ aosp_soong:
 	echo '    "Uses_qcom_bsp_legacy": $(if $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)),true,false),'; \
 	echo '    "Has_legacy_mmap": $(if $(filter true,$(BOARD_USES_LEGACY_MMAP)),true,false),'; \
 	echo '    "Needs_prelink_support": $(if $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)),true,false),'; \
+	echo '    "Needs_non_pie_support": $(if $(filter true,$(TARGET_NEEDS_NON_PIE_SUPPORT)),true,false),'; \
 	echo '    "Uses_qti_camera_device": $(if $(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)