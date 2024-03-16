e23d6f0948a7dad1741b11b939d4fe7271022457
someone5678 <someone5678@users.noreply.github.com>
2024-03-16 22:28:03 +0900

    parasite: vendorsetup.sh: Move KernelSU and KProfiles setup to here
    * Do not make multiple deivces to setup multiple time
    
    Change-Id: I10c0699594dee0003aaf864256f6292e28c2cd31

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
-# Shebang is intentionally missing - do not run as a script
+#!/bin/bash
 
-if [ -z $VENDOR_AOSP_SETUP_DONE ]; then
+if [ -z $VENDOR_PARASITE_SETUP_DONE ]; then
 
 # ABI compatibility checks fail for several reasons:
 #   - The update to Clang 12 causes some changes, but no breakage has been
@@ -16,5 +16,39 @@ if [ -z $VENDOR_AOSP_SETUP_DONE ]; then
 # For now, just skip the ABI checks to fix build errors.
 export SKIP_ABI_CHECKS=true
 
-export VENDOR_AOSP_SETUP_DONE=true
+# Clone Kernel Modules repo
+if [ ! -d "kernel/modules" ]; then
+    mkdir -p kernel/modules
+    touch kernel/modules/Android.mk
+fi
+
+# Clone KernelSU repo
+if [ ! -d "kernel/modules/misc/KernelSU" ]; then
+    git clone https://github.com/tiann/KernelSU kernel/modules/misc/KernelSU
+fi
+
+# Update KernelSU repo
+if [ -d "kernel/modules/misc/KernelSU" ]; then
+    cd kernel/modules/misc/KernelSU
+    git reset --hard
+    git fetch origin
+    git pull origin main
+    cd ../../../../
+fi
+
+# Clone Kprofiles repo
+if [ ! -d "kernel/modules/misc/Kprofiles" ]; then
+    git clone https://github.com/dakkshesh07/Kprofiles kernel/modules/misc/Kprofiles
+fi
+
+# Update Kprofiles repo
+if [ -d "kernel/modules/misc/Kprofiles" ]; then
+    cd kernel/modules/misc/Kprofiles
+    git reset --hard
+    git fetch origin
+    git pull origin main
+    cd ../../../../
+fi
+
+export VENDOR_PARASITE_SETUP_DONE=true
 fi
