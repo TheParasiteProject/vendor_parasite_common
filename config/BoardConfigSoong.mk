ba9515d81ae9c75ad5158648f804007058e66705
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-05-19 14:02:13 +0000

    soong: Add support for miui camera mode [2/2]
    Change-Id: I0c912a1c7056bb204a24bd5f0a08308cae71f3f3

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -7,7 +7,7 @@ SOONG_CONFIG_lineageGlobalVars += \
     camera_uses_newer_hidl_override_format \
     disable_bluetooth_le_read_buffer_size_v2 \
     disable_bluetooth_le_set_host_feature \
-    include_miui_camera \
+    uses_miui_camera \
     needs_camera_boottime \
     needs_oplus_tag \
     target_alternative_futex_waiters \
@@ -28,7 +28,7 @@ SOONG_CONFIG_lineageGlobalVars_camera_needs_miui_camera_mode_support := $(TARGET
 SOONG_CONFIG_lineageGlobalVars_camera_uses_newer_hidl_override_format := $(TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT)
 SOONG_CONFIG_lineageGlobalVars_disable_bluetooth_le_read_buffer_size_v2 := $(TARGET_DISABLE_BLUETOOTH_LE_READ_BUFFER_SIZE_V2)
 SOONG_CONFIG_lineageGlobalVars_disable_bluetooth_le_set_host_feature := $(TARGET_DISABLE_BLUETOOTH_LE_SET_HOST_FEATURE)
-SOONG_CONFIG_lineageGlobalVars_include_miui_camera := $(TARGET_INCLUDES_MIUI_CAMERA)
+SOONG_CONFIG_lineageGlobalVars_uses_miui_camera := $(TARGET_USES_MIUI_CAMERA)
 SOONG_CONFIG_lineageGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_lineageGlobalVars_needs_oplus_tag := $(TARGET_NEEDS_OPLUS_VENDOR_TAG)
 SOONG_CONFIG_lineageGlobalVars_target_alternative_futex_waiters := $(TARGET_ALTERNATIVE_FUTEX_WAITERS)
