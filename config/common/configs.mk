5bc6e913cc69f2e520d55a0c217e605cbd10e164
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-02-14 19:01:06 +0900

    vendor: config: Enable Freeform window management for all devices
    * Check whether device is Android Go
    
    Change-Id: I3848db0abdf0c3c38f606b5201176e94e416da08

--- config/common/configs.mk
+++ config/common/configs.mk
@@ -1,3 +1,9 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(VENDOR_PARASITE_COMMON_DIR)/misc/keylayout/Vendor_2dc8_Product_6006.kl:$(TARGET_COPY_OUT_PRODUCT)/usr/keylayout/Vendor_2dc8_Product_6006.kl
+
+ifneq ($(PRODUCT_TYPE),go)
+# Freeform window management
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.software.freeform_window_management.xml
+endif
