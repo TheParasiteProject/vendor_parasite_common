567432d7178f0c25bde36ceb1e375eb54105d583
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-02-27 18:18:14 +0800

    core: improve hide developer options feature
    basically does what this module does:
    https://github.com/accelforce/DevOptsHide/blob/master/app/src/main/java/net/accelf/devoptshide/HideDevOpts.kt
    to reduce calls and unnecessary looping for adb/developer hidden apps, intercept every method and do an early return instead of looping over get methods also add getString since some apps like Gcash is weird enough to use getString for an integer value.
    https://github.com/accelforce/DevOptsHide/blob/master/app/src/main/java/net/accelf/devoptshide/HideDevOpts.kt#L63
    
    avoid recursion and cleanup code
    
    test
    - without patch: open gcash with developer and usb debugging enabled. gcash detects and crashes
    - with patch: same procedure, gcash doesn't detect anything, gcash works as intended
    
    Change-Id: I8214c2f2642d38e97ab8d193fde8b5dcddc58a57
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- libs/com.android.internal.util.custom/src/HideDeveloperStatusUtils.java
+++ libs/com.android.internal.util.custom/src/HideDeveloperStatusUtils.java
@@ -1,11 +1,11 @@
 package com.android.internal.util.custom;
 
 import android.content.ContentResolver;
-import android.content.Context;
 import android.os.SystemProperties;
 import android.provider.Settings;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,104 +13,38 @@ import java.util.Set;
  * @hide
  */
 public class HideDeveloperStatusUtils {
-    private static final Set<String> settingsToHide =
-            new HashSet<>(
-                    Arrays.asList(
-                            Settings.Global.ADB_ENABLED,
-                            Settings.Global.ADB_WIFI_ENABLED,
-                            Settings.Global.DEVELOPMENT_SETTINGS_ENABLED));
-
-    enum Action {
-        ADD,
-        REMOVE,
-        SET
-    }
+    private static final Set<String> SETTINGS_TO_HIDE =
+            Collections.unmodifiableSet(
+                    new HashSet<>(
+                            Arrays.asList(
+                                    Settings.Global.ADB_ENABLED,
+                                    Settings.Global.ADB_WIFI_ENABLED,
+                                    Settings.Global.DEVELOPMENT_SETTINGS_ENABLED)));
 
     private static boolean isBootCompleted() {
         return SystemProperties.getBoolean("sys.boot_completed", false);
     }
 
     public static boolean shouldHideDevStatus(ContentResolver cr, String packageName, String name) {
-        if (cr == null || packageName == null || name == null || !isBootCompleted()) {
+        if (cr == null
+                || packageName == null
+                || name == null
+                || !isBootCompleted()
+                || !SETTINGS_TO_HIDE.contains(name)) {
             return false;
         }
-
         Set<String> apps = getApps(cr);
-        if (apps.isEmpty()) {
-            return false;
-        }
-
-        return apps.contains(packageName) && settingsToHide.contains(name);
-    }
-
-    private static Set<String> getApps(Context context) {
-        if (context == null) {
-            return new HashSet<>();
-        }
-
-        return getApps(context.getContentResolver());
+        return !apps.isEmpty() && apps.contains(packageName);
     }
 
     private static Set<String> getApps(ContentResolver cr) {
         if (cr == null) {
-            return new HashSet<>();
+            return Collections.emptySet();
         }
-
         String apps = Settings.Secure.getString(cr, Settings.Secure.HIDE_DEVELOPER_STATUS);
-        if (apps != null && !apps.isEmpty() && !apps.equals(",")) {
-            return new HashSet<>(Arrays.asList(apps.split(",")));
+        if (apps == null || apps.isEmpty() || apps.equals(",")) {
+            return Collections.emptySet();
         }
-
-        return new HashSet<>();
-    }
-
-    private static void putAppsForUser(
-            Context context, String packageName, int userId, Action action) {
-        if (context == null || userId < 0) {
-            return;
-        }
-
-        final Set<String> apps = getApps(context);
-        switch (action) {
-            case ADD:
-                apps.add(packageName);
-                break;
-            case REMOVE:
-                apps.remove(packageName);
-                break;
-            case SET:
-                // Don't change
-                break;
-        }
-
-        Settings.Secure.putStringForUser(
-                context.getContentResolver(),
-                Settings.Secure.HIDE_DEVELOPER_STATUS,
-                String.join(",", apps),
-                userId);
-    }
-
-    public void addApp(Context mContext, String packageName, int userId) {
-        if (mContext == null || packageName == null || userId < 0) {
-            return;
-        }
-
-        putAppsForUser(mContext, packageName, userId, Action.ADD);
-    }
-
-    public void removeApp(Context mContext, String packageName, int userId) {
-        if (mContext == null || packageName == null || userId < 0) {
-            return;
-        }
-
-        putAppsForUser(mContext, packageName, userId, Action.REMOVE);
-    }
-
-    public void setApps(Context mContext, int userId) {
-        if (mContext == null || userId < 0) {
-            return;
-        }
-
-        putAppsForUser(mContext, null, userId, Action.SET);
+        return Collections.unmodifiableSet(new HashSet<>(Arrays.asList(apps.split(","))));
     }
 }
