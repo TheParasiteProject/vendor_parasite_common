c04f2902afe322a04fb9b7d09aa4ad42c9007871
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-05-24 09:06:34 +0900

    vendor: Deprecates vendor/parasite/certification
    * It is just waste of time.
    
    Change-Id: Ic7f3dedcda0f26e816db5a69eceea10607cd5113

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,8 +2,5 @@
 VENDOR_PARASITE_COMMON_DIR := vendor/parasite/common
 include $(VENDOR_PARASITE_COMMON_DIR)/config/BoardConfig.mk
 
-# Parasite Certification
-include vendor/parasite/certification/BoardConfig.mk
-
 # Sepolicies
 -include device/parasite/sepolicy/common/sepolicy.mk
--- config.mk
+++ config.mk
@@ -2,9 +2,6 @@
 VENDOR_PARASITE_COMMON_DIR := vendor/parasite/common
 $(call inherit-product, $(VENDOR_PARASITE_COMMON_DIR)/config/common.mk)
 
-# Parasite Certification
-$(call inherit-product-if-exists, vendor/parasite/certification/config.mk)
-
 # Parasite Signatures
 $(call inherit-product-if-exists, vendor/parasite/signatures/config.mk)
 
