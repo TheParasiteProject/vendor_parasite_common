PixelExperience/vendor_aosp

BoardConfigQcom: Initialize Board variables before adding it to list
Change-Id: I85808e2b6b0ce7ebe6d522869e4c88035288667a

config/BoardConfigQcom.mk
@@ -16,9 +16,6 @@ UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET) $(ATOLL)
 UM_4_19_KONA_FAMILY := $(KONA) $(LITO)
 UM_4_19_BENGAL_FAMILY := $(BENGAL)
 UM_5_4_FAMILY := $(LAHAINA) $(HOLI)
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
-LEGACY_UM_PLATFORMS := msm8953 msm8953 msm8996 msm8998 sdm660 $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
-QSSI_SUPPORTED_PLATFORMS := $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
 
 ifeq (,$(TARGET_ENFORCES_QSSI))
 UM_3_18_FAMILY += msm8937 msm8953
@@ -27,6 +24,10 @@ else
 UM_4_9_LEGACY_FAMILY := msm8937 msm8953
 UM_4_19_LEGACY_FAMILY := sdm660
 endif
+
+UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
+LEGACY_UM_PLATFORMS := msm8953 msm8953 msm8996 msm8998 sdm660 $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
+QSSI_SUPPORTED_PLATFORMS := $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
 
 BOARD_USES_ADRENO := true
 

TheParasiteProject/vendor_parasite_common

config/BoardConfigQcom.mk
@@ -16,9 +16,6 @@ UM_4_14_FAMILY := $(MSMNILE) $(MSMSTEPPE) $(TRINKET) $(ATOLL)
 UM_4_19_KONA_FAMILY := $(KONA) $(LITO)
 UM_4_19_BENGAL_FAMILY := $(BENGAL)
 UM_5_4_FAMILY := $(LAHAINA) $(HOLI)
-UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
-LEGACY_UM_PLATFORMS := msm8937 msm8953 msm8996 msm8998 sdm660 $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
-QSSI_SUPPORTED_PLATFORMS := $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
 
 ifeq (,$(TARGET_ENFORCES_QSSI))
 UM_3_18_FAMILY += msm8937 msm8953
@@ -27,6 +24,10 @@ else
 UM_4_9_LEGACY_FAMILY := msm8937 msm8953
 UM_4_19_LEGACY_FAMILY := sdm660
 endif
+
+UM_PLATFORMS := $(UM_3_18_FAMILY) $(UM_4_4_FAMILY) $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
+LEGACY_UM_PLATFORMS := msm8937 msm8953 msm8996 msm8998 sdm660 $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
+QSSI_SUPPORTED_PLATFORMS := $(UM_4_9_LEGACY_FAMILY) $(UM_4_9_FAMILY) $(UM_4_14_FAMILY) $(UM_4_19_LEGACY_FAMILY) $(UM_4_19_KONA_FAMILY) $(UM_4_19_BENGAL_FAMILY) $(UM_5_4_FAMILY)
 
 BOARD_USES_ADRENO := true
 