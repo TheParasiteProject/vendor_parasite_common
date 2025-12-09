LineageOS/android_vendor_lineage

config: Ship EmergencyInfo on WiFi only tablets
Change-Id: I89e16f716076d5351949c0bb13f55fc4eaeee4f5

config/common_full_tablet_wifionly.mk
@@ -10,3 +10,5 @@ PRODUCT_PACKAGES += \
 # Include Lineage LatinIME dictionaries
 PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/dictionaries
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/lineage/overlay/dictionaries
+
+$(call inherit-product, vendor/lineage/config/wifionly.mk)

config/wifionly.mk
@@ -1,0 +1,5 @@
+# Apps
+PRODUCT_PACKAGES += \
+    EmergencyInfo
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/lineage/overlay/wifionly

TheParasiteProject/vendor_parasite_common

config/common_full_tablet_wifionly.mk
@@ -5,3 +5,4 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 
 # Inherit full common PixelExperience stuff
 $(call inherit-product, vendor/aosp/config/common_full.mk)
+$(call inherit-product, vendor/aosp/config/wifionly.mk)

config/wifionly.mk
@@ -1,0 +1,5 @@
+# Apps
+PRODUCT_PACKAGES += \
+    EmergencyInfo
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/wifionly