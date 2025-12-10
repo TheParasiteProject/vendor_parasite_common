6d809c5999158f0604d7422f323b6cdf40ee6de1
someone5678 <59456192+someone5678@users.noreply.github.com>
2025-12-11 03:10:19 +0900

    vendor: Use "-if-exists" for all makefile call
    Change-Id: I4781997f124d9d57d334a1869fa89b225f4a6398

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # Parasite Configuration
 VENDOR_PARASITE_COMMON_DIR := vendor/parasite/common
-$(call inherit-product, $(VENDOR_PARASITE_COMMON_DIR)/config/common.mk)
+$(call inherit-product-if-exists, $(VENDOR_PARASITE_COMMON_DIR)/config/common.mk)
 
 # Parasite Signatures
 $(call inherit-product-if-exists, vendor/parasite/signatures/config.mk)
@@ -14,10 +14,10 @@ $(call inherit-product-if-exists, vendor/parasite/proprietary/config.mk)
 # GMS
 TARGET_CUSTOM_APEX_CERTIFICATE_DIR := \
     $(abspath $(TOP)/vendor/parasite/signatures/common/data)
-$(call inherit-product, vendor/google/gms/products/gms.mk)
+$(call inherit-product-if-exists, vendor/google/gms/products/gms.mk)
 
 # Sony
-$(call inherit-product, vendor/sony/somc/products/somc.mk)
+$(call inherit-product-if-exists, vendor/sony/somc/products/somc.mk)
 
 # Parasite Test Suite
 $(call inherit-product-if-exists, vendor/parasite/testsuite/config.mk)
